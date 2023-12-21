159a9567585f1a5320a953e975c1f976c430336c
Raiq04 <118801562+Raiq04@users.noreply.github.com>
2023-12-21 13:29:27 +0400

    not finished

---
+++ LoginRegisterApp.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LoginRegisterApp extends JFrame {
+
+    private CardLayout cardLayout;
+    private JPanel cardPanel;
+
+    public LoginRegisterApp() {
+        initializeUI();
+    }
+
+    private void initializeUI() {
+
+        
+    }
+
+}
