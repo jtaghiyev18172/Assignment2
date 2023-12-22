2af320cabe8c0ae688d86be56afa64c69279f051
Raiq04 <118801562+Raiq04@users.noreply.github.com>
2023-12-22 18:52:45 +0400

    login and register created

--- LoginRegisterApp.java
+++ LoginRegisterApp.java
@@ -13,8 +13,44 @@ public class LoginRegisterApp extends JFrame {
     }
 
     private void initializeUI() {
+        setTitle("Login");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400, 300);
+        setLocationRelativeTo(null);
+        setResizable(false);
 
-        
+        cardLayout = new CardLayout();
+        cardPanel = new JPanel(cardLayout);
+
+        // Login Panel
+        JPanel loginPanel = createLoginPanel();
+        cardPanel.add(loginPanel, "login");
+
+        // Register Panel
+        JPanel registerPanel = createRegisterPanel();
+        cardPanel.add(registerPanel, "register");
+
+        add(cardPanel);
+
+        JButton loginButton = new JButton("Login Here !");
+        JButton registerButton = new JButton("Register Here !");
+        loginButton.setVisible(false);
+
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                registerButton.setVisible(true);
+                loginButton.setVisible(false);
+                cardLayout.show(cardPanel, "login");
+            }
+        });
     }
 
+    private JPanel createRegisterPanel() {
+        return null;
+    }
+
+    private JPanel createLoginPanel() {
+        return null;
+    }
 }
