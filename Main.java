18a297922bf7942552b053f7d79446f564d04c1d
jtaghiyev18172 <119033156+jtaghiyev18172@users.noreply.github.com>
2024-01-03 00:02:33 +0400

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-       User guest1 = new User(null, null);
-       guest1.register("Jake090", "tommygun07");
-       System.out.println(guest1.login("Jake090", "tommygun07"));
-       
-    }
-}
